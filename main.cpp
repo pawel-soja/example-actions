2b2adb9443b75cfbcb09c1b3f1977fb05ee6f121
Paweł Soja <pawel.soja@egzotech.com>
2022-12-23 22:44:41 +0100

    wip

--- main.cpp
+++ main.cpp
@@ -5,5 +5,18 @@ int main(int, char *[])
 {
     std::cout << "Hello World!" << std::endl;
     std::cout << "zlib version: " << zlibVersion() << std::endl;
+
+#ifdef HAVE_TIMESPEC_GET
+    std::cout << "HAVE_TIMESPEC_GET: yes" << std::endl;
+#else
+    std::cout << "HAVE_TIMESPEC_GET: no" << std::endl;
+#endif
+
+#ifdef HAVE_CLOCK_GETTIME
+    std::cout << "HAVE_CLOCK_GETTIME: yes" << std::endl;
+#else
+    std::cout << "HAVE_CLOCK_GETTIME: no" << std::endl;
+#endif
+
     return 0;
 }
